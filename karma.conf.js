7104eecc10e4a186ebffc5944a1e3815083c0e48
Julie Ralph <ju.ralph@gmail.com>
2016-04-28 23:13:33 -0700

    Almost working tests - rxjs and local files serving

--- karma.conf.js
+++ karma.conf.js
@@ -7,26 +7,32 @@ module.exports = function(config) {
 
     files: [
       // System.js for module loading
-      'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
+      // 'node_modules/systemjs/dist/system-polyfills.js',
+      // 'node_modules/systemjs/dist/system.src.js',
 
       // Polyfills.
       'node_modules/es6-shim/es6-shim.js',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
+      // 'node_modules/angular2/bundles/angular2-polyfills.js',
 
       // Zone.js dependencies
       // Note - do not include zone.js itself here, it is already
       // included in angular2-polyfills
+      'node_modules/zone.js/dist/zone.js',
       'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
 
+      { pattern: 'node_modules/reflect-metadata/Reflect.js', included: true, watched: false },
+
       // RxJs.
-      'node_modules/rxjs/bundles/Rx.js',
+      { pattern: 'https://code.angularjs.org/tools/system.js', included: true, watched: false },
+      // { pattern: 'https://code.angularjs.org/2.0.0-beta.16/Rx.js', included: true, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
 
       // Angular 2 itself and the testing library.
-      'node_modules/angular2/bundles/angular2.js',
-      'node_modules/angular2/bundles/testing.dev.js',
+      // 'node_modules/angular2/bundles/angular2.js',
+      // 'node_modules/angular2/bundles/testing.dev.js',
 
 
       {pattern: 'karma-test-shim.js', included: true, watched: true},
@@ -34,6 +40,9 @@ module.exports = function(config) {
 
       // paths loaded via module imports
       {pattern: 'built/**/*.js', included: false, watched: true},
+
+      {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
+      {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: true},
 
       // paths loaded via Angular's component compiler
       // (these paths need to be rewritten, see proxies section)
